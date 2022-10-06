bbefe312fb3df1e635f0b87b4590e1257c4bce67
Faiz E Arsal <93216129+FaizArsal@users.noreply.github.com>
2022-10-06 11:40:49 +0500

    wot

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Form } from 'react-bootstrap';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import './App.css';
+import Search from "./Search";
+
+function App() {
+  return (<>
+  
+   
+   <Search/>
+   </>
+  );
+}
+
+export default App;
